2a747cb5ba5b3506fc576d613cb95387cefda5db
Li Qiang <liq3ea@163.com>
2017-08-03 16:34:05 +0800

    delete 163 emails in batches.

---
+++ deleteEmail.py
@@ -0,0 +1,16 @@
+# delete the 163 emails 
+# use Snipaste to find the coordinates
+# pip install PyUserInput
+
+import pymouse, pykeyboard, os, sys
+import time
+from pymouse import *
+from pykeyboard import PyKeyboard
+
+m = PyMouse()
+
+for i in range(300):
+    m.click(224,172,1,1)
+    time.sleep(0.5)
+    m.click(284,173,1,1)
+    time.sleep(2)
